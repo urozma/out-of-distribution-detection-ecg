a62c696532f2784819101811489b57257b13865b
Urska Rozman <urska.rozman@student.tugraz.at>
2023-08-24 22:48:42 +0200

    feat: matrix

--- signal_generator.py
+++ signal_generator.py
@@ -8,10 +8,8 @@ def peaks_vector(length, peaks):
     vector[np.random.choice(length, peaks, replace=False)] = 1
 
     for i, peak in enumerate(vector):
-        if peak == 1:
-
-            #peaks are randomly chosen between 20 and 50
-            vector[i] = random.randrange(20, 50, 1)
+        if peak == 1:       
+            vector[i] = random.randrange(20, 50)
 
     return vector
 
@@ -28,11 +26,31 @@ def gaussian_kernel(signal, mean, std):
     
     return pseudo_signal
 
+def add_noise(signal, scale):
+    noise = np.random.normal(0, scale, len(signal))
+    noisy_signal = signal + noise
+    return noisy_signal
+
+
 # test for five peaks
 original_signal = peaks_vector(1000, 5)
 pseudo_signal = gaussian_kernel(original_signal, 0, 10)
-print(pseudo_signal)
+noisy_signal = add_noise(pseudo_signal, 0.1)
 
-plt.figure()
-plt.plot(range(len(pseudo_signal)),pseudo_signal)
-plt.show()
+#plt.figure()
+#plt.plot(range(len(noisy_signal)),noisy_signal)
+#plt.show()
+
+
+def matrix(samples, length, peaks):
+    matrix = np.empty((samples, length))
+    for s in range(samples):
+        original_signal = peaks_vector(length, peaks)
+        pseudo_signal = gaussian_kernel(original_signal, 0, 1)
+        noisy_signal = add_noise(pseudo_signal, 0.1)
+
+        matrix = matrix + noisy_signal
+    return matrix
+
+matrix = matrix(10, 1000, 5)
+print(matrix)
